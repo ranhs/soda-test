42f09f20b8a0258222e506e0673d730e8ee153d3
Ran Har-Shuv <ran.harshuv@gmail.com>
2021-09-16 14:32:40 +0300

    restore controlMEthods2.test to the version where order of describe/it was known

--- lib/controlMethods2.test.ts
+++ lib/controlMethods2.test.ts
@@ -1,99 +1,67 @@
 import { expect, describe, context, it, TR, beforeEach, afterEach, before, after } from '.'
 
-let BM=0,BEM=0,AEM=0,AM=0,B1=0,BE1=0,AE1=0,A1=0,B2=0,BE2=0,AE2=0,A2=0
-let T1=0,T2=0,T3=0,T4=0,T5=0,T=0,TC1=0,TC2=0
-
-function expectValue(args: number[],value: number): void {
-    for (const arg of args) {
-        expect(arg).to.equal(value)
-    }
-}
+let afterWasCalled = false
 
 @describe("Control Methods 2")
 class CtrlMethods2Test {
+    calls: string[] = []
 
     before(): void {
-        // nothing called until now
-        expectValue([BM,BEM,AEM,AM,B1,BE1,AE1,A1,B2,BE2,AE2,A2],0)
-        expectValue([T1,T2,T3,T4,T5],0)
-        BM++
+        this.calls.push('BM')
     }
 
     beforeEach(): void {
-        expectValue([BM],1)
-        expectValue([AEM-BEM,AM,AE1-BE1,AE2-BE2],0)
-        BEM++
-        T=0
+        this.calls.push('BEM')
     }
 
     afterEach(): void {
-        expectValue([BM,BEM-AEM,T],1)
-        expectValue([AM,BE1-AE1,BE2-AE2],0)
-        AEM++
+        this.calls.push('AEM')
     }
 
     after(): void {
-        expectValue([BM],1)
-        expectValue([AM,AEM-BEM,A1-B1,AE1-BE1,A2-B2,AE2-BE2],0)
-        expectValue([T1,T2,T3,T4,T5],1)
-        AM++
+        this.calls.push('AM')
+        expect(this.calls).to.deep.equal(['AE2','AEM','A2','AM'])
+        afterWasCalled = true
     }
 
     @it()
     test1(): TR {
-        expectValue([BM,BEM-AEM],1)
-        expectValue([AM,T1,T,A1-B1,AE1-BE1,A2-B2,AE2-BE2],0)
-        T1++
-        T++
+        expect(this.calls).to.deep.equal(['BM','BEM'])
+        this.calls = []
     }
 
 @context('context1')
 
     @beforeEach()
     beforeEach1(): void {
-        expectValue([BM,B1],1)
-        expectValue([AM,A1,AE1-BE1,A2-B2,AE2-BE2,T],0)
-        TC1=0
-        BE1++
+        this.calls.push('BE1')
     }
 
     @afterEach()
     afterEach1(): void {
-        expectValue([BM,B1,BE1-AE1,T,TC1],1)
-        expectValue([AM,A1,B2-A2,BE2-AE2],0)
-        AE1++
+        this.calls.push('AE1')
     }
 
     @before()
     before1(): void {
-        expectValue([BM],1)
-        expectValue([AM,B1,A1,BE1-AE1,B2-A2,BE2-AE2,T2,T3],0)
-        B1++
+        this.calls.push('B1')
     }
 
     @after()
     after1(): void {
-        expectValue([BM,B1,T2,T3],1)
-        expectValue([AM,A1,BEM-AEM,BE1-AE1,B2-A2,BE2-AE2],0)
-        A1++
+        this.calls.push('A1')
     }
 
     @it()
     test2(): TR {
-        expectValue([BM,B1,BEM-AEM,BE1-AE1],1)
-        expectValue([AM,A1,B2-A2,BE2-AE2,T,TC1,T2],0)
-        T++
-        TC1++
-        T2++
+        expect(this.calls).to.deep.equal(['AEM','B1','BEM','BE1'])
+        this.calls = []
     }
 
     @it()
     test3(): TR {
-        expectValue([BM,B1,BEM-AEM,BE1-AE1],1)
-        expectValue([AM,A1,B2-A2,BE2-AE2,T,TC1,T3],0)
-        T++
-        TC1++
-        T3++
+        expect(this.calls).to.deep.equal(['AE1','AEM','BEM','BE1'])
+        this.calls = []
     }
 
     
@@ -101,49 +69,34 @@ class CtrlMethods2Test {
 
     @beforeEach()
     beforeEach2(): void {
-        expectValue([BM,B2],1)
-        expectValue([AM,A2,AE1-BE1,A1-B1,AE2-BE2,T],0)
-        TC2=0
-        BE2++
+        this.calls.push('BE2')
     }
 
     @afterEach()
     afterEach2(): void {
-        expectValue([BM,B2,BE2-AE2,T,TC2],1)
-        expectValue([AM,A2,B1-A1,BE1-AE1],0)
-        AE2++
+        this.calls.push('AE2')
     }
 
     @before()
     before2(): void {
-        expectValue([BM],1)
-        expectValue([AM,B2,A2,BE1-AE1,B1-A1,BE2-AE2,T4,T5],0)
-        B2++
+        this.calls.push('B2')
     }
 
     @after()
     after2(): void {
-        expectValue([BM,B2,T4,T5],1)
-        expectValue([AM,A2,BEM-AEM,BE1-AE1,B1-A1,BE2-AE2],0)
-        A2++
+        this.calls.push('A2')
     }
 
     @it()
     test4(): TR {
-        expectValue([BM,B2,BEM-AEM,BE2-AE2],1)
-        expectValue([AM,A2,B1-A1,BE1-AE1,T,TC2,T4],0)
-        T++
-        TC2++
-        T4++
+        expect(this.calls).to.deep.equal(['AE1','AEM','A1','B2','BEM','BE2'])
+        this.calls = []
     }
 
     @it()
     test5(): TR {
-        expectValue([BM,B2,BEM-AEM,BE2-AE2],1)
-        expectValue([AM,A2,B1-A1,BE1-AE1,T,TC2,T5],0)
-        T++
-        TC2++
-        T5++
+        expect(this.calls).to.deep.equal(['AE2','AEM','BEM','BE2'])
+        this.calls = []
     }
 
 }
@@ -152,6 +105,6 @@ class CtrlMethods2Test {
 class ConsoleMethods3Test {
     @it('should be after controlMethods2 after call')
     validateAfterWasCalled(): TR {
-        expect(AM).to.equal(1)
+        expect(afterWasCalled).to.be.true
     }
 }
