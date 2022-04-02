1ecd7c32358d8aaea3a4129bd18ff536c0e4a1a4
Ran Har-Shuv <ran.harshuv@gmail.com>
2022-04-02 15:47:00 +0300

    support new path of karma builders in angular13

--- build-angular/karma/index.ts
+++ build-angular/karma/index.ts
@@ -1,5 +1,10 @@
 import * as rewire from '../../test-lib/rewire'
-const karma = require('../../../../@angular-devkit/build-angular/src/karma') // eslint-disable-line @typescript-eslint/no-var-requires
+let karma
+try {
+    karma = require('../../../../@angular-devkit/build-angular/src/builders/karma') // eslint-disable-line @typescript-eslint/no-var-requires
+} catch {
+    karma = require('../../../../@angular-devkit/build-angular/src/karma') // eslint-disable-line @typescript-eslint/no-var-requires
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 rewire.init(true)
 exports.execute = karma.execute
